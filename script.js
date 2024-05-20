5b8c83e6ea01fc2e3268df6770a006964ae4a0ce
Abdallah A. Alwarawreh <66683380+Abdallah-Alwarawreh@users.noreply.github.com>
2024-05-20 07:16:39 +0000

    Update

--- script.js
+++ script.js
@@ -176,16 +176,15 @@ function onPointerDown(event) {
 
 var ImgsGroup;
 var imgs = [
-	"https://media.discordapp.net/attachments/1080351232570433567/1239615693238833213/Appicons_20211230143951.png?ex=66498054&is=66482ed4&hm=bd1595f26b5af6904444ce1cec5466f00b908b8e878c4ad4e2fe21a2b412eb2c&",
-	"https://media.discordapp.net/attachments/1080351232570433567/1241178579484803182/TotemOfAnni_20240517190014.png?ex=66494121&is=6647efa1&hm=fb52ace20734261f3e2919e7352ea5dd89ffdcb5c8711b36e70d3562b3a6fc63&",
-	"https://media.discordapp.net/attachments/960756578838011904/1234289282936737842/SampleEmotes_20240428184353.png?ex=66493d79&is=6647ebf9&hm=2fbf8684bb314bac97673baa2419db6aa581ea565121b3cb268ecb7d687ba370&",
-	"https://media.discordapp.net/attachments/1080351232570433567/1224522976712724481/PFPS_20240401173424.png?ex=66494e64&is=6647fce4&hm=f3ef3e96921b56fbe74e2f3735cf5be62f08556edc28cb6147ee3993b9336de7&",
-	"https://media.discordapp.net/attachments/1080351232570433567/1221726586433306644/Untitled554_20240325021356.png?ex=6649ae0c&is=66485c8c&hm=f610289704be286e2bc29ae23b818b8c5a663dc7cebb37ecc83bf50037586650&",
-	"https://media.discordapp.net/attachments/960756578838011904/1210821497170432011/Sketches_20240223232457.png?ex=66498ee4&is=66483d64&hm=448a7ca707726af8c50185fafd3345e88c5c73cba302952a2fb0acdb501e2929&",
-	"https://media.discordapp.net/attachments/1080351232570433567/1208606922136227890/PFPS_20240217203716.png?ex=66496969&is=664817e9&hm=a2ed8f0c00648653392a465b128ff5399cb58e6f955dd4e36da31fb7880bd0a5&",
-	"https://media.discordapp.net/attachments/960756578838011904/1192583755680989326/Kikocord3_20240104153939.png?ex=6649c96c&is=664877ec&hm=07dfe2af733c1aa676f631b0e05100a3df5afdfd848b2b079ce43a11ec80c411&",
-	"https://media.discordapp.net/attachments/960756578838011904/1192257876773834772/Kikocord3_20240103180521.png?ex=664942ac&is=6647f12c&hm=fbea4e40d37cab38570ab4de850f7ead3718abf1f8862ea9fb1c2e4c5addec11&",
-	"https://media.discordapp.net/attachments/1080351232570433567/1183614338515341373/Untitled502_20231210212521.png?ex=664974c0&is=66482340&hm=378b8f00a89440c9154bffb26a8184a878828728afc3a6ce15ccf64b49c9034c&",
+    "https://media.discordapp.net/attachments/1080351232570433567/1239615693238833213/Appicons_20211230143951.png?ex=66498054&is=66482ed4&hm=bd1595f26b5af6904444ce1cec5466f00b908b8e878c4ad4e2fe21a2b412eb2c&",
+    "https://media.discordapp.net/attachments/1080351232570433567/1241178579484803182/TotemOfAnni_20240517190014.png?ex=66494121&is=6647efa1&hm=fb52ace20734261f3e2919e7352ea5dd89ffdcb5c8711b36e70d3562b3a6fc63&",
+    "https://media.discordapp.net/attachments/960756578838011904/1234289282936737842/SampleEmotes_20240428184353.png?ex=66493d79&is=6647ebf9&hm=2fbf8684bb314bac97673baa2419db6aa581ea565121b3cb268ecb7d687ba370&",
+    "https://media.discordapp.net/attachments/1080351232570433567/1224522976712724481/PFPS_20240401173424.png?ex=66494e64&is=6647fce4&hm=f3ef3e96921b56fbe74e2f3735cf5be62f08556edc28cb6147ee3993b9336de7&",
+    "https://media.discordapp.net/attachments/1080351232570433567/1221726586433306644/Untitled554_20240325021356.png?ex=6649ae0c&is=66485c8c&hm=f610289704be286e2bc29ae23b818b8c5a663dc7cebb37ecc83bf50037586650&",
+    "https://media.discordapp.net/attachments/960756578838011904/1210821497170432011/Sketches_20240223232457.png?ex=66498ee4&is=66483d64&hm=448a7ca707726af8c50185fafd3345e88c5c73cba302952a2fb0acdb501e2929&",
+    "https://media.discordapp.net/attachments/1080351232570433567/1208606922136227890/PFPS_20240217203716.png?ex=66496969&is=664817e9&hm=a2ed8f0c00648653392a465b128ff5399cb58e6f955dd4e36da31fb7880bd0a5&",
+    "https://media.discordapp.net/attachments/960756578838011904/1192583755680989326/Kikocord3_20240104153939.png?ex=6649c96c&is=664877ec&hm=07dfe2af733c1aa676f631b0e05100a3df5afdfd848b2b079ce43a11ec80c411&",
+    "https://media.discordapp.net/attachments/960756578838011904/1192257876773834772/Kikocord3_20240103180521.png?ex=664942ac&is=6647f12c&hm=fbea4e40d37cab38570ab4de850f7ead3718abf1f8862ea9fb1c2e4c5addec11&",
 ];
 
 function loadimages(){
@@ -197,16 +196,23 @@ function loadimages(){
 	const textureloader = new THREE.TextureLoader();
 	for (let i = 0; i < imgs.length; i++){
 		textureloader.load(imgs[i], function (texture) {
-			const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+			const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true, side: THREE.DoubleSide });
+			
 			// plane
-			const geometry = new THREE.PlaneGeometry(7, 7);
+			//const geometry = new THREE.PlaneGeometry(7, 7);
+			const geometry = RoundedRectangle(7,7,2,5)
 			const mesh = new THREE.Mesh(geometry, material);
+			
 			mesh.position.set(Math.cos(i * (Math.PI * 2 / imgs.length)) * 25, 0, Math.sin(i * (Math.PI * 2 / imgs.length)) * 25);
 			mesh.rotation.set(0, 0, Math.PI * 2 / imgs.length);
 			mesh.lookAt(gift.position);
 			
 			ImgsGroup.add(mesh);
 			
+		}, function ( xhr ) {
+			console.log( (xhr.loaded / xhr.total * 100) + '% loaded ' + imgs[i]);
+		}, function ( error ) {
+			console.error(error);
 		});
 	}
 	scene.add(ImgsGroup);
@@ -228,3 +234,76 @@ function render() {
 	renderer.render(scene, camera);
 	TWEEN.update();
 };
+
+
+
+
+// non indexed BufferGeometry
+
+function RoundedRectangle( w, h, r, s ) { // width, height, radius corner, smoothness  
+	
+	// helper const's
+	const wi = w / 2 - r;		// inner width
+	const hi = h / 2 - r;		// inner height
+	const w2 = w / 2;			// half width
+	const h2 = h / 2;			// half height
+	const ul = r / w;			// u left
+	const ur = ( w - r ) / w;	// u right
+	const vl = r / h;			// v low
+	const vh = ( h - r ) / h;	// v high
+	
+	let positions = [
+	
+		-wi, -h2, 0,  wi, -h2, 0,  wi, h2, 0,
+		-wi, -h2, 0,  wi,  h2, 0, -wi, h2, 0,
+		-w2, -hi, 0, -wi, -hi, 0, -wi, hi, 0,
+		-w2, -hi, 0, -wi,  hi, 0, -w2, hi, 0,
+		 wi, -hi, 0,  w2, -hi, 0,  w2, hi, 0,
+		 wi, -hi, 0,  w2,  hi, 0,  wi, hi, 0
+		
+	];
+	
+	let uvs = [
+		
+		ul,  0, ur,  0, ur,  1,
+		ul,  0, ur,  1, ul,  1,
+		 0, vl, ul, vl, ul, vh,
+		 0, vl, ul, vh,  0, vh,
+		ur, vl,  1, vl,  1, vh,
+		ur, vl,  1, vh,	ur, vh 
+		
+	];
+	
+	let phia = 0; 
+	let phib, xc, yc, uc, vc, cosa, sina, cosb, sinb;
+	
+	for ( let i = 0; i < s * 4; i ++ ) {
+	
+		phib = Math.PI * 2 * ( i + 1 ) / ( 4 * s );
+		
+		cosa = Math.cos( phia );
+		sina = Math.sin( phia );
+		cosb = Math.cos( phib );
+		sinb = Math.sin( phib );
+		
+		xc = i < s || i >= 3 * s ? wi : - wi;
+		yc = i < 2 * s ? hi : -hi;
+	
+		positions.push( xc, yc, 0, xc + r * cosa, yc + r * sina, 0,  xc + r * cosb, yc + r * sinb, 0 );
+		
+		uc =  i < s || i >= 3 * s ? ur : ul;
+		vc = i < 2 * s ? vh : vl;
+		
+		uvs.push( uc, vc, uc + ul * cosa, vc + vl * sina, uc + ul * cosb, vc + vl * sinb );
+		
+		phia = phib;
+			
+	}
+	
+	const geometry = new THREE.BufferGeometry( );
+	geometry.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
+	geometry.setAttribute( 'uv', new THREE.BufferAttribute( new Float32Array( uvs ), 2 ) );
+	
+	return geometry;
+	
+}
