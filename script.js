b9754bc7b1100cebea455b4f659a360a04568bf7
Abdallah A. Alwarawreh <66683380+Abdallah-Alwarawreh@users.noreply.github.com>
2024-05-22 19:01:30 +0000

    Music & Collage redirect

--- script.js
+++ script.js
@@ -91,6 +91,21 @@ const font = floader.load(
 			document.getElementById("loading").style.display = "none";
 		}, 1000)
 
+		const listener = new THREE.AudioListener();
+		camera.add( listener );
+		
+		// create a global audio source
+		const sound = new THREE.Audio( listener );
+
+		// load a sound and set it as the Audio object's buffer
+		const audioLoader = new THREE.AudioLoader();
+		audioLoader.load( 'music.mp3', function( buffer ) {
+			sound.setBuffer( buffer );
+			sound.setLoop( true );
+			sound.setVolume( 0.5 );
+			sound.play();
+		});
+
 		window.addEventListener('click', onPointerDown);
 		render();
 	},
@@ -165,7 +180,7 @@ function onPointerDown(event) {
 								.start()
 								.onComplete(() => {
 									// redierct to gift
-									window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+									window.location.href = "https://cdn.discordapp.com/attachments/1237619515655720980/1242910456793006213/Untitled10_20240522214159.png?ex=664f8e11&is=664e3c91&hm=00e6d4e65d97f8afff3c1647e6d2577e59b0af765c8b5faaff12f4692a12022b&";
 								})
 						})
 				})
